204eb77965f2882cf3666c3696902e989743dbe7
Lammert Westerhoff <westerhoff@gmail.com>
2020-03-10 10:23:25 +0100

    Initial Commit

---
+++ AssetMaker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AssetMaker
+//
+//  Created by Lammert Westerhoff on 10/03/2020.
+//  Copyright © 2020 Lammert Westerhoff. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
